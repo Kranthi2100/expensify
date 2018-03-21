0498f0a71af4418ab577d1ad4d40e99e3bcdf0be
Kranthi2100 <kranthi.alluri2100@gmail.com>
2018-03-21 23:31:30 +0530

    heroku ready

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-
+const port = process.env.PORT || 3000;
 app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
